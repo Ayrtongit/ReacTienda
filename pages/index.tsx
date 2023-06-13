671af606d517e692e647720066f1447ef61d2f1a
AyrCoder <pau.fernandezpy@gmail.com>
2023-06-13 08:19:50 -1200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import Head from "next/head";
+import React from "react";
+import Navbar from "../components/Navbar"
+export default function Home() {
+
+  return (
+    <div>
+        
+      <Head>
+        <title>eCommerce</title>
+        <meta name="description" content="Index" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      
+
+
+         <div className="transition flex justify-center duration-700 text-center">
+         <Navbar></Navbar>
+         </div>
+      
+
+
+ 
+
+
+
+
+            
+          
+        
+ 
+    </div>
+  );
+}
